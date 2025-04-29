7005147225a1b90e248eed33726cb492240b9fb4
thales-vicente <thalesvicentebem10@hotmail.com>
2025-04-29 17:47:34 -0300

    add text to toolbar

--- app/src/main/java/com/example/superapp/presentation/components/AppToolbar.kt
+++ app/src/main/java/com/example/superapp/presentation/components/AppToolbar.kt
@@ -1,16 +1,22 @@
 package com.example.superapp.presentation.components
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.superapp.R
 
 @Composable
@@ -19,14 +25,20 @@ fun AppToolbar(){
         modifier = Modifier
             .fillMaxWidth()
             .height(70.dp)
-            .padding(18.dp)
+            .padding(18.dp),
+        verticalAlignment = Alignment.CenterVertically
     ){
         Icon(
             modifier = Modifier.size(40.dp),
             painter = painterResource(id = R.drawable.ic_user),
             contentDescription = "User Image"
         )
-
+        Spacer(modifier = Modifier.width(18.dp))
+        TextComponent(
+            modifier = Modifier.wrapContentSize(),
+            textValue = stringResource(R.string.add_address),
+            fontSizeValue = 20.sp
+        )
     }
 }
 @Preview(showBackground = true)
