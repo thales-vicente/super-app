92e5ee1cf83d51f5475377bd3ffe5ecbe27c3a60
thales-vicente <thalesvicentebem10@hotmail.com>
2025-04-29 18:09:02 -0300

    icon positions adjust

--- app/src/main/java/com/example/superapp/presentation/components/AppToolbar.kt
+++ app/src/main/java/com/example/superapp/presentation/components/AppToolbar.kt
@@ -1,11 +1,13 @@
 package com.example.superapp.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Icon
@@ -18,32 +20,39 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.superapp.R
+import com.example.superapp.presentation.theme.primaryColor
+import com.example.superapp.presentation.theme.whiteColor
 
 @Composable
 fun AppToolbar(){
     Row (
         modifier = Modifier
+            .background(primaryColor)
+            .systemBarsPadding()
             .fillMaxWidth()
             .height(70.dp)
-            .padding(18.dp),
+            .padding(start = 18.dp, end = 18.dp, top = 8.dp),
         verticalAlignment = Alignment.CenterVertically
     ){
         Icon(
-            modifier = Modifier.size(40.dp),
+            modifier = Modifier.size(28.dp),
             painter = painterResource(id = R.drawable.ic_user),
-            contentDescription = "User Image"
+            contentDescription = "User Image",
+            tint = whiteColor
         )
         Spacer(modifier = Modifier.width(18.dp))
         TextComponent(
             modifier = Modifier.wrapContentSize(),
             textValue = stringResource(R.string.add_address),
-            fontSizeValue = 20.sp
+            fontSizeValue = 20.sp,
+            textColorValue = whiteColor
         )
         Spacer(modifier = Modifier.weight(1f))
         Icon(
-            modifier = Modifier.size(40.dp),
+            modifier = Modifier.size(28.dp),
             painter = painterResource(id = R.drawable.ic_notifications),
-            contentDescription = "Notification Image"
+            contentDescription = "Notification Image",
+            tint = whiteColor
         )
     }
 }
