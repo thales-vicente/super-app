ad668d0b89ec586495a9348c35cde5ed6197aa46
thales-vicente <thalesvicentebem10@hotmail.com>
2025-04-29 17:51:52 -0300

    add toolbar to HomeScreen

--- app/src/main/java/com/example/superapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/superapp/presentation/screens/home/HomeScreen.kt
@@ -3,22 +3,32 @@ package com.example.superapp.presentation.screens.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.superapp.presentation.components.AppToolbar
 import com.example.superapp.presentation.theme.whiteColor
 
 @Composable
 fun HomeScreen(){
-    Column (
-        modifier = Modifier.fillMaxSize()
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
-            .background(whiteColor)
-    ){
+    Scaffold (
+        topBar = {
+            AppToolbar()
+        }
+    ) { innerPadding ->
 
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+                .background(whiteColor)
+                .padding(innerPadding)
+        ) {
+        }
     }
 }
 @Preview(showBackground = true)
